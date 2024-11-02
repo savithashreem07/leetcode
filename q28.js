6b5ea1b4370db4c4c51f0b6f3913c61500e64d64
Savitha Shree <157434708+savithashreem07@users.noreply.github.com>
2024-11-02 16:00:16 -0700

    Solve Leetcode Q28

--- q28.js
+++ q28.js
@@ -4,5 +4,18 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle)
+    // return haystack.indexOf(needle)
+
+    // Alternate solution using loops and substring
+    if (needle.length === 0) {
+        return 0;
+    }
+
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        if (haystack.substring(i, i + needle.length) === needle) {
+            return i;
+        }
+    }
+
+    return -1;
 };
