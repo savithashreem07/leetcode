500ef724330515a829d2f9c5c6cfcdf3701b44d6
Savitha Shree <157434708+savithashreem07@users.noreply.github.com>
2024-11-09 22:12:04 -0800

    Solve Leetcode Q92

---
+++ q92.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} left
+ * @param {number} right
+ * @return {ListNode}
+ */
+var reverseBetween = function(head, left, right) {
+
+    if (!head || left == right) return head
+
+    let dummy = new ListNode(0)
+    dummy.next = head
+    let prev = dummy
+
+    for (i=1; i<left; i++) {
+        prev = prev.next
+    }
+
+    let current = prev.next
+    let next = current.next
+
+    for (i=0; i < right - left; i++) {
+        current.next = next.next;
+        next.next = prev.next;
+        prev.next = next;
+        next = current.next;
+    }
+
+    return dummy.next
+    
+};
